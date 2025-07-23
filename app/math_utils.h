671955d4f508a610588238d71f26d0dee281b13e
Igor Serdyuchenko <iserdyuchenko@sonatype.com>
2025-07-23 17:31:07 +0300

    Update code to C

---
+++ app/math_utils.h
@@ -0,0 +1,9 @@
+#ifndef MATH_UTILS_H
+#define MATH_UTILS_H
+
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+double divide(int a, int b);
+
+#endif // MATH_UTILS_H
